4b26444bf2b721146b472c8a630a443be9f09899
khjj567 <123910845+khjj567@users.noreply.github.com>
2023-05-25 00:25:52 +0900

    5월 25일 새벽
    복구됐나

--- src/main/java/com/example/restcontroller/RestMemberController.java
+++ src/main/java/com/example/restcontroller/RestMemberController.java
@@ -20,6 +20,41 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class RestMemberController {
+    final JwtUtil jwtUtil; // Component 객체 생성
+    final BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder(); // 객체 생성
+    final MemberRepository mRepository;
 
+    //127.0.0.1:9090/CUSTOM/api/member/login.json
+    @PostMapping(value="/login.json")
+    public Map<String, Object> loginPOST(@RequestBody Member member) {
+    Map<String, Object> retMap = new HashMap<>();
+        try {
+            // 1. 이메일, 암호 전송 확인
+            
+
+            // 2. 이메일을 이용해서 정보를 가져옴.
+            Member retmember = mRepository.findById(member.getMid()).orElse(null);
+            log.info("회원정보 => {}", retmember.toString());
+            // // 3. 실패시 전송할 데이터
+            // retMap.put( "status", 0 );
+
+
+            // // 4. 암호가 일치하는지 확인 => 전송된 hash되지 않은 암호와 DB에 해시된 암호 일치 확인
+            // if(  bcpe.matches( member.getMpw(), retmember.getMpw()) ) {
+                retMap.put( "status", 200 );
+                retMap.put( "token", jwtUtil.createJwt(retmember.getMid(), retmember.getMname() ) );
+
+                //  암호가 일치하고 로그인요건을 충족하면 200과 토큰(jwtUtil이 )
+            
+            // } 
+
+        } catch (Exception e) {
+            e.printStackTrace(); 
+            retMap.put( "status", -1 );
+            retMap.put( "error", e.getMessage() );
+        }
+        return retMap;
+    }
+    
    
 }
